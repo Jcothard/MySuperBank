b5671de9af3e4658b8a7751ee32d7261d1f2b2ae
Josh Cothard <jcothard@yahoo.com>
2020-03-03 21:08:52 -0500

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("jj", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
